ae43f3411dbe1749a001992ab6978017cee4e940
Nikolay Malyutin <xenos-nick@mail.ru>
2022-01-02 20:03:35 +0300

    Написана функция получения данных о пользователе

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from random import randrange
+import requests
 
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-from config import token
+from config import token, version
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
 
+url = 'https://api.vk.com/method/'
+params = {
+            'access_token': token,
+            'v': version,
+        }
+
+
+def get_user_data(user_id):
+    get_user_data_params = {
+        'user_ids': user_id,
+        'fields': 'city, sex, relation, bdate'
+    }
+    data_get_url = url + 'users.get'
+    user_data = requests.get(data_get_url, params={**params, **get_user_data_params}).json()
+    return user_data
+    # user_sex = user_data['response'][0].get('sex')
+
+
+get_user_data('141040434')
 
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
